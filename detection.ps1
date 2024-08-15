e0e284f5f9d314d994604b87472803f086f30204
Sérgio Sant'Ana Júnior <sesantanajr@gmail.com>
2024-08-15 20:09:41 -0300

    detection.ps1

--- detection.ps1
+++ detection.ps1
@@ -4,7 +4,7 @@
 $targetPath = "C:\Windows\Web\Wallpaper\Intune"
 
 # Lista de arquivos para verificar
-$files = @("papeldeparede.jpg", "papeldeparede2.jpg")
+$files = @("papeldeparede.jpg", "teladebloqueio.jpg")
 
 $allFilesPresent = $true
 
