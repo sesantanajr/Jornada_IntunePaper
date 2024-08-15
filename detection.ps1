b570fde81a45744c5799ae22a1780a32b1ec30c0
Sérgio Sant'Ana Júnior <sesantanajr@gmail.com>
2024-08-15 18:55:24 -0300

    DetectWallpaper.ps1

--- detection.ps1
+++ detection.ps1
@@ -1,29 +1,26 @@
-$packageId = "Postman.Postman"
-$version = "11.6.1"
+# Script de Detecção
 
-$wingetOutput = & "winget" "list" "--id" $packageId "--exact" "--disable-interactivity" "--accept-source-agreements"
+# Caminho de destino
+$targetPath = "C:\Windows\Web\Wallpaper\Intune"
 
-if($wingetOutput -is [array]) {
-    $lastRow = $wingetOutput[$wingetOutput.Length -1]
-    if ($lastRow.Contains("$packageId $version")) {
-        Write-Host "$($packageId) version $($version) is installed"
-        Exit 0
-    } elseif ($lastRow.Contains($packageId)) {
-        [reflection.assembly]::LoadWithPartialName("System.Version")
-        $i = $lastRow.IndexOf(" $packageId ") + $packageId.Length + 1
-        $nextSpace = $lastRow.IndexOf(" ", $i + 1)
-        $installedVersion = $lastRow.Substring($i+1, $nextSpace - $i -1)
-        $versionExpected = New-Object System.Version($version)
-        $versionInstalled = New-Object System.Version($installedVersion)
-        Write-Host "$($packageId) version $($installedVersion) is installed"
-        $result = $versionExpected.CompareTo($versionInstalled);
-        if (1 -eq $result) {
-            Write-Host "Installed version is lower"
-            Exit 5
-        }
-        Exit 0
+# Lista de arquivos para verificar
+$files = @("setta_combustiveis_agosto.jpg", "papeldeparede.jpg")
+
+$allFilesPresent = $true
+
+foreach ($file in $files) {
+    $filePath = Join-Path $targetPath $file
+    if (-not (Test-Path $filePath)) {
+        Write-Host "Arquivo $file não encontrado"
+        $allFilesPresent = $false
+        break
     }
 }
 
-Write-Host "$($packageId) not detected using winget"
-Exit 10
+if ($allFilesPresent) {
+    Write-Host "Todos os arquivos estão presentes"
+    exit 0  # Sucesso
+} else {
+    Write-Host "Nem todos os arquivos estão presentes"
+    exit 1  # Falha
+}
